752a16b951da5485702063bcb26063d6ba0acf40
Chace Powell <cpcpowell8@gmail.com>
2018-11-28 23:35:23 -0500

    add and configure web server. add React skeleton. todo: add models, controller, and routes and test API.

---
+++ models/report.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reportSchema = new Schema({
+  
+});
+
+const Report = mongoose.model('Report', reportSchema);
+
+module.exports = Report;
