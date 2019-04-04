326ec4679c21fb158b46c0687f78922da2061f88
Chace Powell <cpcpowell8@gmail.com>
2019-04-04 15:06:35 -0400

    refactor models and controllers for mongo usage. connect to local mongodb in server.js. completely remove all mysql usage.

--- controller/reportController.js
+++ controller/reportController.js
@@ -1,45 +1,24 @@
 const db = require("../models");
 
 module.exports = {
-  findAll: (req, res) => {
-    db.Report
-      .findAll({ include: [ db.Round ]})
-      .then(dbReports => res.json(dbReports))
-      .catch(err => status(422).json(err));
-  },
-
-  findOne: (req, res) => {
-    db.Report
-      .findOne(
-        { 
-          where: { id: req.params.id }, 
-          include: [ db.Round ]
-        }
-      )
-      .then(dbReport => res.json(dbReport))
-      .catch(err => res.status(422).json(err));
-  },
 
   create: (req, res) => {
-    db.Report
-      .create(req.body)
-      .then(dbReport => res.json(dbReport))
-      .catch(err => res.status(422).json(err));
-  },
-
-  update: (req, res) => {
-    db.Report
-      .update( req.body, {
-        where: { id: req.body.id }
+    db.Report.create( req.body )
+      .then(newReport => {
+        return db.User.findOneAndUpdate(
+          { uid: req.params.uid },
+          { report: newReport._id },
+          { new: true }
+        )
       })
-      .then(dbReport => res.json(dbReport))
-      .catch(err => res.status(422).json(err));
+      .then(updatedUser => res.json(updatedUser))
+      .catch(err => res.status(422).json(err))
   },
 
-  destroy: (req, res) => {
-    db.Report
-      .destroy({ where: { id: req.body.id }})
-      .then(dbReport => res.json(dbReport))
-      .catch(err => res.status(422).json(err));
+  findAll: (req, res) => {
+    db.Report.find({})
+      .then(reports => res.json(reports))
+      .catch(err => res.status(422).json(err))
   }
+
 };
--- controller/userController.js
+++ controller/userController.js
@@ -2,57 +2,15 @@ const db = require('../models');
 
 module.exports = {
 
-  findOne: (req, res) => {
-    db.User.findOne({
-      where: { uid: req.params.id },
-      include: [
-        {
-          model: db.Report,
-          include: [
-            {
-              model: db.Round,
-              include: [
-                {
-                  model: db.sideboardRecommendation
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    })
-      .then(foundUser => res.json(foundUser))
-      .catch(err => res.status(422).json(err));
-  },
-
-  findOneReports: (req, res) => {
-    db.User.findOne({
-      where: { fbId: req.params.id },
-      include: [
-        {
-          model: db.Report
-        }
-      ]
-    })
-    .then(foundReports => res.json(foundReports))
-    .catch(err => res.status(422).json(err));
-  },
-
   create: (req, res) => {
-    db.User
-      .create(req.body)
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+    db.User.create( req.body )
   },
 
-  findOrCreate: (req, res) => {
-    db.User.findOrCreate({
-      where: { uid: req.params.id },
-      defaults: {
-        userName: req.body.userName
-      }
-    })
-      .then(dbUser => res.json(dbUser))
-      .catch(err => res.status(422).json(err));
+  findOneAndPopulate: (req, res) => {
+    db.User.fineOne({ uid: req.body.uid })
+      .populate('report')
+      .then(userReports => res.json(userReports))
+      .catch(err => res.status(422).json(err))
   }
+
 };
